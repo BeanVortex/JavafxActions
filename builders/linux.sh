f931a3e266b0f07f298476cb7e1fe0b0953c49ac
DarkDeveloper_Plant <goldeagle00110011@outlook.com>
2023-07-14 21:05:04 +0330

    linux ci

--- builders/linux.sh
+++ builders/linux.sh
@@ -22,11 +22,13 @@ do
 
   $JAVA_HOME/bin/jpackage \
   --type $type \
-  --dest ./build/releases \
+  --dest build/releases \
+  --input build/libs/ \
   --name ActionsDemo \
   --main-class com.javafx.actionsgradledemo.Main \
+  --main-jar ${MAIN_JAR} \
   --java-options '-Djdk.gtk.version=2' \
-#  --runtime-image ./build/image \
+  --runtime-image build/image \
   --icon src/main/resources/com/javafx/actionsgradledemo/icons/logo.png \
   --linux-shortcut \
   --linux-menu-group "ActionsDemo" \
